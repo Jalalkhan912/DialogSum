43d817f2f6ce02702ccee9fe17899f8a941b561f
Jalal Khan <118057500+Jalalkhan912@users.noreply.github.com>
2024-10-25 13:45:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -96,13 +96,13 @@ def few_shot(example_indices_full_few_shot,my_example):
 
 st.title("FLAN-T5(Base) Prompt Engineered: Zero-shot, Single-shot, and Few-shot")
 
-my_example = st.text_area("Enter dialogues to summarize", value="#Maaz#: Jalal how are you?#Jalal#:  I am good thank you.#Maaz#: Are you going to school tomorrow.#Jalal#: No bro i am not going to school tomorrow.#Maaz#: why? #Jalal#: I am working on a project, are you want to work with me on my project?#Maaz#: sorry, i have to go to school.")
+my_example = st.text_area("Enter dialogues to summarize")
 
 if st.button("Run"):  
     zero_shot_output = zero_shot(my_example)
     one_shot_output = one_shot(example_indices_full,my_example)
     few_shot_output = few_shot(example_indices_full_few_shot,my_example)
-    st.header("Comparizion of Outputs")
-    st.write(f"**Zero-shot Output:**\n{zero_shot_output}\n")
-    st.write(f"**Single-shot Output:**\n{one_shot_output}\n")
-    st.write(f"**Few-shot Output:**\n{few_shot_output}\n")
+    st.header("\n\n*Comparizion of Outputs*\n\n")
+    st.write(f"*Zero-shot Output:*{zero_shot_output}\n\n")
+    st.write(f"*Single-shot Output:*{one_shot_output}\n\n")
+    st.write(f"*Few-shot Output:*{few_shot_output}\n")
