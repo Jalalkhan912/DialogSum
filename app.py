450f1508b93c9d9164ba3567d8d81dc9c5fd07d9
Jalal Khan <118057500+Jalalkhan912@users.noreply.github.com>
2024-10-25 13:42:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ st.title("FLAN-T5(Base) Prompt Engineered: Zero-shot, Single-shot, and Few-shot"
 my_example = st.text_area("Enter dialogues to summarize", value="#Maaz#: Jalal how are you?#Jalal#:  I am good thank you.#Maaz#: Are you going to school tomorrow.#Jalal#: No bro i am not going to school tomorrow.#Maaz#: why? #Jalal#: I am working on a project, are you want to work with me on my project?#Maaz#: sorry, i have to go to school.")
 
 if st.button("Run"):  
-    summary, zero_shot_output = zero_shot(my_example)
+    zero_shot_output = zero_shot(my_example)
     one_shot_output = one_shot(example_indices_full,my_example)
     few_shot_output = few_shot(example_indices_full_few_shot,my_example)
     st.header("Comparizion of Outputs")
